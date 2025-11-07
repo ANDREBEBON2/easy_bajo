687e3a0676bf21a034cb6f680037d58923b70c86
ANDREBEBON2 <andrebebon21@gmail.com>
2025-11-07 17:07:37 +0800

    Make Home Page & Navbar Comp

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,34 @@
 <script setup></script>
 
 <template>
-  <main>
-    <h1 class="text-3xl font-bold underline text-red-300"></h1>
+  <main
+    class="py-52 flex flex-col gap-6 items-center justify-center text-center text-white min-h-screen"
+  >
+    <h1 class="text-6xl font-bold block">Explore the Beauty of Labuan Bajo</h1>
+    <h2 class="block">
+      Discover unforgettable travel experiences with exotic destinations and the best accommodations
+      in Labuan Bajo
+    </h2>
+
+    <!-- Button -->
+    <div class="flex gap-6 mt-10">
+      <button class="h-10 px-10 bg-orange-500 text-white rounded-full font-semibold">
+        Explore Destination
+      </button>
+      <button class="h-10 px-10 bg-green-500 text-white rounded-full font-semibold">
+        WhatsApp
+      </button>
+    </div>
   </main>
 </template>
+
+<style scoped>
+main {
+  background-image:
+    linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('../../public/Images/bg.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-color: black;
+}
+</style>
